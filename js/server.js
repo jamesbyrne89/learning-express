ac9994f1698ccf30ee30cc0f8d1a44db19e7491f
jamesbyrne89 <mail@jamestbyrne.com>
2018-05-01 23:01:24 +0100

    Add route to cities JSON

--- js/server.js
+++ js/server.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const cities = require('./cities')
+
+const app = express();
+
+app.get('/data', (req, resp) => {
+    resp.send(cities)
+})
+
+app.listen(3000)
